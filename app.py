869cd397957553f32d439491c7af917cd5c6871b
Tomoyuki Yoshikawa <yosshyjungle@gmail.com>
2025-04-28 15:12:05 +0900

    2nd commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+model = pickle.load(open('iris.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def home():
+    try:
+        data1 = request.form['sl']
+        data2 = request.form['sw']
+        data3 = request.form['pl']
+        data4 = request.form['pw']
+        arr = np.array([[data1, data2, data3, data4]])
+        pred = model.predict(arr)
+        return render_template('after.html', data=pred)
+    except Exception as error:
+        print(error)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
---
+++ iris_tensorflow2.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow import keras
+from sklearn import datasets
+iris = datasets.load_iris()
+
+# print(iris.DESCR)
+# iris.data
+# iris.target
+
+from sklearn.model_selection import train_test_split as split
+x_train, x_test, y_train, y_test = split(iris.data, iris.target, test_size=0.2)
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_dim=4),
+    keras.layers.Dense(32, activation=tf.nn.relu),
+    keras.layers.Dense(3, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='sgd',
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=100)
+
+score = model.evaluate(x_test, y_test)
+print('loss:', score[0])
+print('accuracy:', score[1])
+model.save('iris_model.h5')
+#一つのデータに対する評価の実行
+# import numpy as np
+# x = np.array([[5.1, 3.5, 1.4, 0.2]])
+# r = model.predict(x)
+# print(r)
+# r.argmax()
+
+
